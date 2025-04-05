a38eb51047900eaa0e09d3327e0cb05390e4fe32
zzuegg <zzuegg@gmail.com>
2025-04-05 06:49:49 +0200

    Implement component retrieval and removal with logging
    Added functionality to retrieve and remove components from the ComponentManager. Introduced Lombok's @Slf4j to provide warning logs when a component is not found or removal fails.

--- engine/core/src/main/java/mylie/engine/core/ComponentManager.java
+++ engine/core/src/main/java/mylie/engine/core/ComponentManager.java
@@ -2,7 +2,8 @@ package mylie.engine.core;
 
 import java.util.LinkedList;
 import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class ComponentManager {
 	private final List<Component> components;
 
@@ -15,10 +16,19 @@ public class ComponentManager {
 	}
 
 	public <T extends Component> T component(Class<T> type) {
-		throw new UnsupportedOperationException("Not yet implemented");
-	}
+		for (Component component : components) {
+			if (type.isAssignableFrom(component.getClass())) {
+				return type.cast(component);
+			}
+		}
+		log.warn("No component of type < {} > found", type.getSimpleName());
+		return null;
+	};
 
 	public <T extends Component> void removeComponent(T component) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		boolean removed = components.remove(component);
+		if (!removed) {
+			log.warn("Component < {} > not found", component.getClass().getSimpleName());
+		}
 	}
 }
