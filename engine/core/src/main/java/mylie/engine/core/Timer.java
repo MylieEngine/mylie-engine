fd25f3885538ee798fc917eb1252571798c158b9
zzuegg <zzuegg@gmail.com>
2025-04-07 10:51:45 +0200

    Refactor Timer to support customizable simulation speed.
    Added a `Settings` class to manage simulation speed and updated the `Timer` to calculate time progress with nanosecond precision. Refactored fields and methods for better clarity and concurrency safety.

--- engine/core/src/main/java/mylie/engine/core/Timer.java
+++ engine/core/src/main/java/mylie/engine/core/Timer.java
@@ -2,15 +2,44 @@ package mylie.engine.core;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
 
 public class Timer extends Component {
+	public static final double NANOS_TO_SECONDS = 1_000_000_000.0;
+
 	@Getter(AccessLevel.PUBLIC)
-	private volatile Time currentTime;
-	private float simulationSpeed = 1.0f;
-	private long simulationTime;
+	private Time currentTime;
+	private long lastUpdate;
+	private Settings settings;
 	protected Timer(ComponentManager manager) {
 		super(manager);
-		simulationTime = System.nanoTime();
-		currentTime = new Time(0, 0, 0, System.nanoTime(), simulationTime);
+		lastUpdate = System.nanoTime();
+		currentTime = new Time(0, 0, 0, 0, 0);
 	}
+
+	protected void update(){
+		long now = System.nanoTime();
+		long elapsed=now-lastUpdate;
+		long newFrameId= currentTime().frameId();
+		long newTimeNanosSimulation= (long) (currentTime.timeNanosSimulation()+(elapsed* settings.simulationSpeed()));
+		newFrameId++;
+		double newDelta=elapsed/1000000000.0;
+		currentTime=new Time(newFrameId,newDelta,newDelta*settings.simulationSpeed(),now,newTimeNanosSimulation);
+	}
+
+	@Getter(AccessLevel.PROTECTED)
+	public static class Settings implements Serializable {
+		@Serial
+		private static final long serialVersionUID = 7692428273946684281L;
+		private float simulationSpeed = 1.0f;
+
+		public Settings(float simulationSpeed) {
+			this.simulationSpeed = simulationSpeed;
+		}
+	}
+
+
 }
