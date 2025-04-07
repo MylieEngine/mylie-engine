8bedf4fc06acb57a356e5cbbd155b308b19c77ff
zzuegg <zzuegg@gmail.com>
2025-04-07 05:10:48 +0200

    Add serialVersionUID to ConstructorNotFoundException
    The serialVersionUID ensures reliable serialization and deserialization of the exception class. This change improves compatibility and prevents potential InvalidClassExceptions during object stream operations.

--- engine/core/src/main/java/mylie/engine/util/exceptions/ConstructorNotFoundException.java
+++ engine/core/src/main/java/mylie/engine/util/exceptions/ConstructorNotFoundException.java
@@ -1,8 +1,11 @@
 package mylie.engine.util.exceptions;
 
+import java.io.Serial;
 import java.util.Arrays;
 
 public class ConstructorNotFoundException extends RuntimeException {
+	@Serial
+	private static final long serialVersionUID = 4646810972584243483L;
 	public ConstructorNotFoundException(Class<?> clazz, Class<?>... parameterTypes) {
 		super("Constructor not found for class " + clazz.getName() + " with parameters "
 				+ Arrays.toString(parameterTypes));
