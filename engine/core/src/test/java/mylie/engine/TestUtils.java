3457e2607cd5278532e89fa330738638229e45bd
zzuegg <zzuegg@gmail.com>
2025-04-07 05:05:34 +0200

    Add utility class `TestUtils` for testing instantiation
    Introduce `TestUtils` to test instantiation logic for utility classes. Prevents unintentional instantiation and verifies proper exception handling with `IllegalInstantiationException`.

---
+++ engine/core/src/test/java/mylie/engine/TestUtils.java
@@ -0,0 +1,21 @@
+package mylie.engine;
+
+import java.lang.reflect.Constructor;
+import mylie.engine.util.exceptions.Exceptions;
+import mylie.engine.util.exceptions.IllegalInstantiationException;
+import org.junit.jupiter.api.Assertions;
+
+public class TestUtils {
+	private TestUtils() {
+
+	}
+	public static void testUtilityInstantiation(Class<?> clazz) {
+		Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
+		constructor.setAccessible(true);
+		Exception exception = Assertions.assertThrows(Exception.class, () -> constructor.newInstance());
+		IllegalInstantiationException instantiationException = (IllegalInstantiationException) Exceptions
+				.getRootCause(exception);
+		Assertions.assertEquals("Class " + clazz.getName() + " cannot be instantiated.",
+				instantiationException.getMessage());
+	}
+}
