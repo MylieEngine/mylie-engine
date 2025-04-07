440ec378e634c6da66216e5a39dc1af4b3181ee2
zzuegg <zzuegg@gmail.com>
2025-04-07 05:04:46 +0200

    Add IllegalInstantiationException to handle invalid instantiations
    Introduce a custom RuntimeException to indicate attempts to instantiate classes that should not be instantiated. This exception provides a clear message and captures the affected class for better debugging.

---
+++ engine/core/src/main/java/mylie/engine/util/exceptions/IllegalInstantiationException.java
@@ -0,0 +1,10 @@
+package mylie.engine.util.exceptions;
+
+public class IllegalInstantiationException extends RuntimeException {
+	private final Class<?> clazz;
+
+	public IllegalInstantiationException(Class<?> clazz) {
+		super("Class " + clazz.getName() + " cannot be instantiated.");
+		this.clazz = clazz;
+	}
+}
