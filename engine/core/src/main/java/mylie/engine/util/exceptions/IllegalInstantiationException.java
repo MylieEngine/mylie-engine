0b7fdb263e8486be000b7eccd3d67197da598c0b
zzuegg <zzuegg@gmail.com>
2025-04-07 05:10:52 +0200

    Add serialVersionUID to IllegalInstantiationException
    This change introduces a serialVersionUID field to the IllegalInstantiationException class to ensure consistent deserialization. It enhances compatibility and prevents potential serialization-related issues.

--- engine/core/src/main/java/mylie/engine/util/exceptions/IllegalInstantiationException.java
+++ engine/core/src/main/java/mylie/engine/util/exceptions/IllegalInstantiationException.java
@@ -1,9 +1,13 @@
 package mylie.engine.util.exceptions;
 
+import java.io.Serial;
 import lombok.Getter;
 
 @Getter
 public class IllegalInstantiationException extends RuntimeException {
+	@Serial
+	private static final long serialVersionUID = 2108472316156304685L;
+
 	private final Class<?> clazz;
 
 	public IllegalInstantiationException(Class<?> clazz) {
