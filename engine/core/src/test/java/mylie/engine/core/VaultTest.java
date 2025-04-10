2630655e168f24d76f52878e0d837d1fcc1f89d3
zzuegg <zzuegg@gmail.com>
2025-04-10 09:34:53 +0200

    Add Vault class and corresponding test cases
    Introduce the Vault class to manage and retrieve stored items with thread-safe operations. Implement unit tests to validate adding, retrieving, and removing items from the Vault, ensuring functionality and correctness.

---
+++ engine/core/src/test/java/mylie/engine/core/VaultTest.java
@@ -0,0 +1,30 @@
+package mylie.engine.core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VaultTest {
+	Vault vault;
+	@BeforeEach
+	public void setUp() {
+		vault = new Vault(null);
+	}
+
+	@AfterEach
+	public void tearDown() {
+		vault = null;
+	}
+
+	@Test
+	public void testAddItem() {
+		Assertions.assertNull(vault.item(String.class));
+		Assertions.assertDoesNotThrow(() -> vault.addItem("test"));
+		Assertions.assertNotNull(vault.item(String.class));
+		Assertions.assertEquals("test", vault.item(String.class));
+		Assertions.assertNull(vault.item(Integer.class));
+		Assertions.assertDoesNotThrow(() -> vault.removeItem("test"));
+		Assertions.assertNull(vault.item(String.class));
+	}
+}
