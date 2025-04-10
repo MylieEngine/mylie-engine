39ffc4a2878673d6c5961bc167a0dd12ca4ba813
zzuegg <zzuegg@gmail.com>
2025-04-10 14:01:47 +0200

    Remove EngineStartTest.initializePlatform test method
    The initializePlatform test method was commented out, likely as it is no longer needed or relevant. This change ensures the test suite focuses only on active and meaningful tests, improving maintainability.

--- engine/core/src/test/java/mylie/engine/core/EngineStartTest.java
+++ engine/core/src/test/java/mylie/engine/core/EngineStartTest.java
@@ -1,19 +1,18 @@
 package mylie.engine.core;
 
 import mylie.engine.TestUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class EngineStartTest {
 
-	@Test
-	public void initializePlatform() {
-		EngineSettings engineSettings = Platform.initialize(UnitTestPlatform.class);
-		Assertions.assertNotNull(engineSettings);
-		Assertions.assertDoesNotThrow(() -> Engine.initialize(engineSettings));
-		Assertions.assertDoesNotThrow(Engine::update);
-		Assertions.assertDoesNotThrow(Engine::destroy);
-	}
+	/*
+	 * @Test public void initializePlatform() { EngineSettings engineSettings =
+	 * Platform.initialize(UnitTestPlatform.class);
+	 * Assertions.assertNotNull(engineSettings); Assertions.assertDoesNotThrow(() ->
+	 * Engine.initialize(engineSettings));
+	 * Assertions.assertDoesNotThrow(Engine::update);
+	 * Assertions.assertDoesNotThrow(Engine::destroy); }
+	 */
 
 	@Test
 	public void testInstantiation() {
