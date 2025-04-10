461834ba6560a6aac1b96643775a107de4b2bcb8
zzuegg <zzuegg@gmail.com>
2025-04-10 09:34:57 +0200

    Refactor Timer initialization and update logic
    Made Timer constructor public and improved initialization with fallback for timer settings. Renamed and refined the update method for better clarity and accuracy. Added a

--- engine/core/src/main/java/mylie/engine/core/Timer.java
+++ engine/core/src/main/java/mylie/engine/core/Timer.java
@@ -1,11 +1,10 @@
 package mylie.engine.core;
 
+import java.io.Serial;
+import java.io.Serializable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.io.Serial;
-import java.io.Serializable;
 
 public class Timer extends Component {
 	public static final double NANOS_TO_SECONDS = 1_000_000_000.0;
@@ -14,33 +13,44 @@ public class Timer extends Component {
 	private Time currentTime;
 	private long lastUpdate;
 	private Settings settings;
-	protected Timer(ComponentManager manager) {
+	public Timer(ComponentManager manager) {
 		super(manager);
+		EngineSettings engineSettings = component(Vault.class).item(EngineSettings.class);
+		if (engineSettings != null && engineSettings.timerSettings() != null) {
+			settings = engineSettings.timerSettings();
+		} else {
+			settings = new Settings();
+			if (engineSettings != null) {
+				engineSettings.timerSettings(settings);
+			}
+		}
 		lastUpdate = System.nanoTime();
 		currentTime = new Time(0, 0, 0, 0, 0);
 	}
 
-	protected void update(){
+	protected void onUpdate() {
 		long now = System.nanoTime();
-		long elapsed=now-lastUpdate;
-		long newFrameId= currentTime().frameId();
-		long newTimeNanosSimulation= (long) (currentTime.timeNanosSimulation()+(elapsed* settings.simulationSpeed()));
+		long elapsed = now - lastUpdate;
+		long newFrameId = currentTime().frameId();
+		long newTimeNanosSimulation = (long) (currentTime.timeNanosSimulation()
+				+ (elapsed * settings.simulationSpeed()));
 		newFrameId++;
-		double newDelta=elapsed/1000000000.0;
-		currentTime=new Time(newFrameId,newDelta,newDelta*settings.simulationSpeed(),now,newTimeNanosSimulation);
-		lastUpdate=now;
+		double newDelta = elapsed / 1000000000.0;
+		currentTime = new Time(newFrameId, newDelta, newDelta * settings.simulationSpeed(), now,
+				newTimeNanosSimulation);
+		lastUpdate = now;
 	}
 
+	@Setter
 	@Getter(AccessLevel.PROTECTED)
 	public static class Settings implements Serializable {
 		@Serial
 		private static final long serialVersionUID = 7692428273946684281L;
-		private float simulationSpeed = 1.0f;
+		private float simulationSpeed;
 
-		public Settings(float simulationSpeed) {
-			this.simulationSpeed = simulationSpeed;
+		public Settings() {
+			this.simulationSpeed = 1.0f;
 		}
 	}
-
 
 }
