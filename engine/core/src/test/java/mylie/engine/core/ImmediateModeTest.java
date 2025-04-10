4382866afdc2b5fd418130a8b1ffd7f0ddc097f2
zzuegg <zzuegg@gmail.com>
2025-04-10 14:01:54 +0200

    Add unit tests for `Engine.ImmediateMode` lifecycle handling
    Introduce comprehensive tests to validate the lifecycle behavior of `Engine.ImmediateMode`, including startup, shutdown, restarting, and exception handling scenarios. This ensures robustness and reliability of the immediate mode functionality.

---
+++ engine/core/src/test/java/mylie/engine/core/ImmediateModeTest.java
@@ -0,0 +1,87 @@
+package mylie.engine.core;
+
+import mylie.engine.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ImmediateModeTest {
+
+	@Test
+	public void testLifecycle() {
+		EngineSettings engineSettings = Platform.initialize(UnitTestPlatform.class);
+		engineSettings.handleRestarts(true);
+		ShutdownReason reason = null;
+		reason = Engine.ImmediateMode.start(engineSettings);
+		Assertions.assertNull(reason);
+		reason = Engine.ImmediateMode.update();
+		Assertions.assertNull(reason);
+		Engine.ImmediateMode.shutdown("OK");
+		reason = Engine.ImmediateMode.update();
+		Assertions.assertNotNull(reason);
+		Assertions.assertTrue(reason instanceof ShutdownReason.Normal);
+		Assertions.assertEquals("OK", ((ShutdownReason.Normal) reason).reason());
+	}
+
+	@Test
+	public void testLifecycleWithException() {
+		EngineSettings engineSettings = Platform.initialize(UnitTestPlatform.class);
+		engineSettings.handleRestarts(true);
+		ShutdownReason reason = null;
+		reason = Engine.ImmediateMode.start(engineSettings);
+		Assertions.assertNull(reason);
+		reason = Engine.ImmediateMode.update();
+		Assertions.assertNull(reason);
+		Engine.ImmediateMode.shutdown(new RuntimeException());
+		reason = Engine.ImmediateMode.update();
+		Assertions.assertNotNull(reason);
+		Assertions.assertTrue(reason instanceof ShutdownReason.Error);
+	}
+
+	@Test
+	public void testNotInitialized() {
+		Assertions.assertThrows(IllegalStateException.class, Engine.ImmediateMode::update);
+		Assertions.assertThrows(IllegalStateException.class, () -> Engine.ImmediateMode.shutdown("OK"));
+		Assertions.assertThrows(IllegalStateException.class, Engine.ImmediateMode::restart);
+		Assertions.assertThrows(IllegalStateException.class,
+				() -> Engine.ImmediateMode.shutdown(new RuntimeException()));
+
+	}
+
+	@Test
+	public void testDoubleStart() {
+		EngineSettings engineSettings = Platform.initialize(UnitTestPlatform.class);
+		Assertions.assertDoesNotThrow(() -> Engine.ImmediateMode.start(engineSettings));
+		Assertions.assertThrows(IllegalStateException.class, () -> Engine.ImmediateMode.start(engineSettings));
+		Engine.ImmediateMode.shutdown("OK");
+		Engine.ImmediateMode.update();
+	}
+
+	@Test
+	public void testHandleRestarts() {
+		EngineSettings engineSettings = Platform.initialize(UnitTestPlatform.class);
+		engineSettings.handleRestarts(true);
+		Assertions.assertNull(Engine.ImmediateMode.start(engineSettings));
+		Engine.ImmediateMode.restart();
+		Assertions.assertNull(Engine.ImmediateMode.update());
+		Engine.ImmediateMode.shutdown("OK");
+		Assertions.assertNotNull(Engine.ImmediateMode.update());
+	}
+
+	@Test
+	public void testHandleRestartsFalse() {
+		EngineSettings engineSettings = Platform.initialize(UnitTestPlatform.class);
+		engineSettings.handleRestarts(false);
+		Assertions.assertNull(Engine.ImmediateMode.start(engineSettings));
+		Engine.ImmediateMode.restart();
+		ShutdownReason reason = Engine.ImmediateMode.update();
+		Assertions.assertTrue(reason instanceof ShutdownReason.Restart);
+		Engine.ImmediateMode.start(((ShutdownReason.Restart) (reason)).engineSettings());
+		Engine.ImmediateMode.shutdown("OK");
+		Assertions.assertNotNull(Engine.ImmediateMode.update());
+	}
+
+	@Test
+	public void testInstantiation() {
+		TestUtils.testUtilityInstantiation(Engine.ImmediateMode.class);
+	}
+}
