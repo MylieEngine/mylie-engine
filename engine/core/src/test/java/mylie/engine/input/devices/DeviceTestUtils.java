44b5ae00e40c03f0c7f7f5ab9d4b26b2f809ed02
zzuegg <zzuegg@gmail.com>
2025-04-21 13:29:45 +0200

    Make DeviceTestUtils a utility class
    Mark the DeviceTestUtils class as final and add a private constructor to prevent instantiation. This enforces its intended usage as a utility class.

--- engine/core/src/test/java/mylie/engine/input/devices/DeviceTestUtils.java
+++ engine/core/src/test/java/mylie/engine/input/devices/DeviceTestUtils.java
@@ -5,7 +5,8 @@ import mylie.engine.input.InputDevice;
 import org.junit.jupiter.api.Assertions;
 
 
-public class DeviceTestUtils {
+public final class DeviceTestUtils {
+    private DeviceTestUtils() {}
     @SuppressWarnings("unused")
     public static <D extends InputDevice<D>, I extends Input<D, V>, V> void testDefaultValues(D device,
 			Input<D, V>[] inputs, V defaultValue) {
