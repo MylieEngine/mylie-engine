10d9ec6922352a01c7daa0fec4a240818acecd3f
zzuegg <zzuegg@gmail.com>
2025-04-22 22:47:23 +0200

    Add Play class and adjust test workflow steps
    Introduce a new `Play` class in the engine package with basic initialization for managing input via `InputManager`. Additionally, reorganize the GitHub Actions test workflow by relocating the "Upload Test Results" step for better sequencing and maintaining consistency.

---
+++ engine/core/src/main/java/mylie/engine/Play.java
@@ -0,0 +1,16 @@
+package mylie.engine;
+
+import mylie.engine.input.InputManager;
+
+public class Play {
+	InputManager inputManager;
+
+	public Play(InputManager inputManager) {
+		this.inputManager = inputManager;
+
+
+
+
+
+	}
+}
